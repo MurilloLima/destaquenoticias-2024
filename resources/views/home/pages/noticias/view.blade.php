26bd189edc1932f2cfd6be92ac42759d327ac5da
Murillo Lima <contato@murillolimadev.com.br>
2024-10-12 10:31:38 -0300

    migration visitaas

--- resources/views/home/pages/noticias/view.blade.php
+++ resources/views/home/pages/noticias/view.blade.php
@@ -29,7 +29,10 @@
                         <ul class="post-tags">
                             <li><i class="lnr lnr-user"></i><a href="single-post.html#">Renata da Silva</a></li>
                             {{-- <li><a href="single-post.html#"><i class="lnr lnr-book"></i><span>20 comments</span></a></li> --}}
-                            <li><i class="lnr lnr-eye"></i>872 Views</li>
+                            <li>
+                                <i class="lnr lnr-eye"></i>
+                                {{-- visualizacoes --}}
+                            </li>
                         </ul>
                         <div class="share-post-box">
                             <ul class="share-box">

f17c7b8f36601ffe93b25947e495e7d278979e0a
Murillo Lima <murillo_smit@hotmail.com>
2024-12-29 22:05:52 -0300

    desc news manu

--- resources/views/home/pages/noticias/view.blade.php
+++ resources/views/home/pages/noticias/view.blade.php
@@ -16,7 +16,7 @@
 
                     <!-- single-post -->
                     <div class="single-post">
-                        <h1 style="font-size: 24px">{{ $data->title }}</h1>
+                        <h1>{{ $data->title }}</h1>
                         <ul class="post-tags">
                             <li><i class="lnr lnr-user"></i><a href="single-post.html#">Administrador</a></li>
                             {{-- <li><a href="single-post.html#"><i class="lnr lnr-book"></i><span>20 comments</span></a></li> --}}

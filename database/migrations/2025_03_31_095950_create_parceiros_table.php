87e2668cfdac52e903f0c0edcd742f5694fe3074
Murillo Lima <murillo_smit@hotmail.com>
2025-03-31 10:10:36 -0300

    migrate parceiro

--- database/migrations/2025_03_31_095950_create_parceiros_table.php
+++ database/migrations/2025_03_31_095950_create_parceiros_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('parceiros', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('fone');
+            $table->string('wt')->nullable();
             $table->timestamps();
         });
     }

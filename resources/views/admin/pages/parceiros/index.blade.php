7a40df195f114838bdc2b21a79de1bbfb8bd310f
Murillo Lima <murillo_smit@hotmail.com>
2025-07-08 09:07:43 -0300

    destroy parceiro admin

--- resources/views/admin/pages/parceiros/index.blade.php
+++ resources/views/admin/pages/parceiros/index.blade.php
@@ -47,8 +47,8 @@
                                             <th>Nome</th>
                                             <th>CPF</th>
                                             <th>Telefone</th>
-                                            <th style="width: 140px">Numero</th>
-                                            {{-- <th style="width: 70px">#</th> --}}
+                                            <th style="width: 40px">Numero</th>
+                                            <th style="width: 70px">#</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -59,12 +59,12 @@
                                                 <td>{{ $item->cpf }}</td>
                                                 <td>{{ $item->tel }}</td>
                                                 <td>{{ $item->number }}</td>
-                                                {{-- <td>
+                                                <td>
                                                     <a href="{{ route('admin.pages.parceiros.destroy', [$item->id]) }}"
                                                         title="Excluir">
                                                         <i class="fas fa-trash"></i>
                                                     </a>
-                                                </td> --}}
+                                                </td>
                                             </tr>
                                         @endforeach
                                     </tbody>
